a459d1dc5943577ec4375400bdcd444bb4b0a8e1
Greg Munilla <gregmunilla@gmail.com>
2021-10-20 00:19:10 -0400

    Set up simple component to write tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./component/header";
+import './app.scss';
 function App() {
   return (
     <div className="App">
-        Test
+      <Header />
     </div>
   );
 }
